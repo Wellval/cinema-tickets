8d33c34f068d83e88367bc4834cdb5baea6628c2
Valentina Masalkova <vrassone@gmail.com>
2021-11-22 20:24:04 +0300

    feat: add session time to movie card

--- src/components/AppMovieCard.vue
+++ src/components/AppMovieCard.vue
@@ -5,11 +5,21 @@
     </div>
     <div class="title">{{ title }}</div>
     <p class="movie-card_plot">{{plot}}</p>
+    <div
+      v-if="this.$store.state.sessions.find(session => session.movieTitle === this.title)"
+    >{{ session.timeslotId }}</div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['url', 'title', 'plot']
+  props: ['url', 'title', 'plot'],
+  computed: {
+    session () {
+      return this.$store.state.sessions.find(
+        session => session.movieTitle === this.title
+      )
+    }
+  }
 }
 </script>
