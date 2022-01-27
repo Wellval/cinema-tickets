b942c0a9011e7ef2918356c809d9caa8fbddd560
Valentina Masalkova <vrassone@gmail.com>
2022-01-27 16:59:37 +0300

    fix: update server.js file

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express();
 
-// Serve all the files in '/dist' directory
 app.use(express.static('dist'));
 
 
-app.listen('https://cinema-tickets-back.herokuapp.com/', function () {
+app.listen(3000, function () {
   console.log('!');
 });
